b8da6901ccee2ee74695b3af51d89cfe6884afbe
Michael Friedrich <m.friedrich3@yahoo.com>
2022-12-29 17:46:47 +0100

    insert project code

--- website/scripts/app.js
+++ website/scripts/app.js
@@ -3,6 +3,9 @@
  * Define Global Variables
  */
 
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 /**
  * End Global Variables
  *
